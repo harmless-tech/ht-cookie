a3382c10eef4e0982540f09f5030d351837c1e94
harmless-tech <28830529+harmless-tech@users.noreply.github.com>
2024-03-01 12:41:03 -0500

    Updates and add prettier

--- src/index.ts
+++ src/index.ts
@@ -3,20 +3,19 @@
  * for a explanation of the options.
  */
 export interface CookieOptions {
-    maxAge?: number,
-    expires?: Date,
-    domain?: string,
-    path?: string,
-    sameSite?: "strict" | "lax" | "none",
-    secure?: boolean,
-    partitioned?: boolean,
+    maxAge?: number;
+    expires?: Date;
+    domain?: string;
+    path?: string;
+    sameSite?: "strict" | "lax" | "none";
+    secure?: boolean;
+    partitioned?: boolean;
 }
 
 /**
  * Allows the setting, getting, and removing of cookies.
  */
 export class Cookie {
-
     /**
      * Sets a cookie.
      *
@@ -25,27 +24,25 @@ export class Cookie {
      * See https://developer.mozilla.org/en-US/docs/web/api/document/cookie#write_a_new_cookie
      * for a explanation of the options and their defaults.
      */
-    public static set(name: string, data: string, options: CookieOptions = {}): void {
-        let cStr: string = encodeURIComponent(name) + "=" + encodeURIComponent(data);
+    public static set(
+        name: string,
+        data: string,
+        options: CookieOptions = {},
+    ): void {
+        let cStr: string =
+            encodeURIComponent(name) + "=" + encodeURIComponent(data);
 
-        if(options.maxAge)
-            cStr += ";max-age=" + options.maxAge;
-        if(options.expires)
+        if (options.maxAge) cStr += ";max-age=" + options.maxAge;
+        if (options.expires)
             cStr += ";expires=" + options.expires.toUTCString();
-        if(options.domain)
-            cStr += ";domain=" + options.domain;
-        if(options.path)
-            cStr += ";path=" + options.path;
-        if(options.secure === undefined || options.secure)
-            cStr += ";secure";
-        if(options.partitioned)
-            cStr += ";partitioned";
+        if (options.domain) cStr += ";domain=" + options.domain;
+        if (options.path) cStr += ";path=" + options.path;
+        if (options.secure === undefined || options.secure) cStr += ";secure";
+        if (options.partitioned) cStr += ";partitioned";
 
         cStr += ";samesite=";
-        if(options.sameSite)
-            cStr += options.sameSite;
-        else
-            cStr += "lax";
+        if (options.sameSite) cStr += options.sameSite;
+        else cStr += "lax";
 
         document.cookie = cStr;
     }
@@ -56,12 +53,11 @@ export class Cookie {
         let cookie: string | null = null;
 
         let position: number = store.search(cName + "=");
-        if(position > -1) {
+        if (position > -1) {
             let cStr: string = store.substring(position);
 
             position = cStr.search(";");
-            if(position > -1)
-                cStr = cStr.substring(0, position);
+            if (position > -1) cStr = cStr.substring(0, position);
 
             cookie = decodeURIComponent(cStr.split("=")[1]);
         }
@@ -82,21 +78,25 @@ export class Cookie {
      * See https://developer.mozilla.org/en-US/docs/web/api/document/cookie#write_a_new_cookie
      * for a explanation of the options and their defaults.
      */
-    public static async aSet(name: string, data: string, options: CookieOptions = {}): Promise<void> {
-        return new Promise<void>(resolve => {
+    public static async aSet(
+        name: string,
+        data: string,
+        options: CookieOptions = {},
+    ): Promise<void> {
+        return new Promise<void>((resolve) => {
             this.set(name, data, options);
             resolve();
         });
     }
 
     public static async aGet(name: string): Promise<string | null> {
-        return new Promise<string | null>(resolve => {
+        return new Promise<string | null>((resolve) => {
             resolve(this.get(name));
         });
     }
 
     public static async aRemove(name: string): Promise<void> {
-        return new Promise<void>(resolve => {
+        return new Promise<void>((resolve) => {
             this.remove(name);
             resolve();
         });
@@ -114,20 +114,25 @@ export class Cookie {
 export class LocalStorage {
     public static set(name: string, data: string, maxAge: number = -1): void {
         let cStr: string = encodeURIComponent(data);
-        if(maxAge > 0) {
+        if (maxAge > 0) {
             const date: Date = new Date();
             date.setHours(date.getHours() + maxAge);
             cStr += ";" + date;
         }
 
-        localStorage.setItem(encodeURIComponent(name), encodeURIComponent(cStr));
+        localStorage.setItem(
+            encodeURIComponent(name),
+            encodeURIComponent(cStr),
+        );
     }
 
     public static get(name: string): string | null {
-        const data: string | null = localStorage.getItem(encodeURIComponent(name));
-        if(data != null) {
+        const data: string | null = localStorage.getItem(
+            encodeURIComponent(name),
+        );
+        if (data != null) {
             const values: Array<string> = decodeURIComponent(data).split(";");
-            if(values.length > 1 && new Date(values[1]) <= new Date()) {
+            if (values.length > 1 && new Date(values[1]) <= new Date()) {
                 localStorage.removeItem(encodeURIComponent(name));
                 return null;
             }
@@ -142,21 +147,25 @@ export class LocalStorage {
         localStorage.removeItem(encodeURIComponent(name));
     }
 
-    public static async aSet(name: string, data: string, maxAge: number = -1): Promise<void> {
-        return new Promise<void>(resolve => {
+    public static async aSet(
+        name: string,
+        data: string,
+        maxAge: number = -1,
+    ): Promise<void> {
+        return new Promise<void>((resolve) => {
             this.set(name, data, maxAge);
             resolve();
         });
     }
 
     public static async aGet(name: string): Promise<string | null> {
-        return new Promise<string | null>(resolve => {
+        return new Promise<string | null>((resolve) => {
             resolve(this.get(name));
         });
     }
 
     public static async aRemove(name: string): Promise<void> {
-        return new Promise<void>(resolve => {
+        return new Promise<void>((resolve) => {
             this.remove(name);
             resolve();
         });
