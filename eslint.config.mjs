9d5bb4bf228de7284f35f1f79aa776c4b6b321ce
harmless-tech <git@harmless.tech>
2025-05-24 15:05:53 -0400

    Update packages and support ES6

---
+++ eslint.config.mjs
@@ -0,0 +1,53 @@
+import { defineConfig } from "eslint/config";
+import typescriptEslint from "@typescript-eslint/eslint-plugin";
+import globals from "globals";
+import tsParser from "@typescript-eslint/parser";
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+import js from "@eslint/js";
+import { FlatCompat } from "@eslint/eslintrc";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const compat = new FlatCompat({
+    baseDirectory: __dirname,
+    recommendedConfig: js.configs.recommended,
+    allConfig: js.configs.all
+});
+
+export default defineConfig([{
+    extends: compat.extends("eslint:recommended", "plugin:@typescript-eslint/recommended"),
+
+    plugins: {
+        "@typescript-eslint": typescriptEslint,
+    },
+
+    languageOptions: {
+        globals: {
+            ...globals.browser,
+        },
+
+        parser: tsParser,
+        ecmaVersion: 6,
+        sourceType: "script",
+    },
+
+    settings: {},
+
+    files: ["src/**.ts"],
+
+    rules: {
+        "@typescript-eslint/no-explicit-any": "off",
+        "@typescript-eslint/explicit-module-boundary-types": "off",
+        "@typescript-eslint/no-inferrable-types": "off",
+
+        indent: ["error", 4, {
+            SwitchCase: 1,
+        }],
+
+        "linebreak-style": 0,
+        quotes: ["error", "double"],
+        semi: ["error", "always"],
+        "eol-last": ["error", "always"],
+    },
+}]);
